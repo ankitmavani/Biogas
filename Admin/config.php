40b2c37387c497eb941116e2ebf9d153b077acfd
ankitmavani <55914708+ankitmavani@users.noreply.github.com>
2022-08-03 22:40:21 +0530

    Update config.php

--- Admin/config.php
+++ Admin/config.php
@@ -1,6 +1,6 @@
 <?php 
 
-$conn=mysqli_connect("localhost","u195877346_bio_gas","pranavpssaavliya1266@","u195877346_root") or die("connection fail/...");
+$conn=mysqli_connect("localhost","","","") or die("connection fail/...");
 
-$hostname = "https://aksharbiotech.in";
+$hostname = "";
 ?>
